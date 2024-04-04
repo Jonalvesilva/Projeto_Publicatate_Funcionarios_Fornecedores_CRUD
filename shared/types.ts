836660703d4d94f9677827c491e238005e2a8d73
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-04-04 08:34:45 -0300

    first commit

---
+++ shared/types.ts
@@ -0,0 +1,16 @@
+export type Funcionario = {
+  id: number;
+  name: string;
+  surname: string;
+  title: string;
+  salary: string;
+  created_at: string;
+};
+
+export type Fornecedor = {
+  id: number;
+  name: string;
+  status: string;
+  person: string;
+  created_at: string;
+};
